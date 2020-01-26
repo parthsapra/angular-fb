7e5df873f9d8f5cdf14972f013d36b8521ec111a
parthsapra <parthsapra91@gmail.com>
2020-01-26 19:35:54 +0530

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import{FbUser} from './fb.model';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'parth';
+  user:FbUser = new FbUser();
+  submit(){
+    console.log(this.user);
+  }
 }
---
+++ src/app/fb.model.ts
@@ -0,0 +1,10 @@
+export class FbUser{
+  firstName:string;
+  lastName:string;
+  email:string;
+  password:string;
+  day:number;
+  month:number;
+  year:number;
+  gender:string;
+}
