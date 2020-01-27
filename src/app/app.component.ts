5e7b5360d275a26b70932e19e4bf20857a87d2f9
parthsapra <parthsapra91@gmail.com>
2020-01-27 12:36:00 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
-import{FbUser} from './fb.model';
+import { Component } from "@angular/core";
+import { FbUser } from "./fb.model";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  user:FbUser = new FbUser();
-  submit(){
-    console.log(this.user);
+export class AppComponent implements OnInit {
+  user: FbUser = new FbUser();
+  errorID1="";
+  userFormGroup: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.userFormGroup = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['',Validators.required],
+      email: ['',Validators.required],
+      password: ['',Validators.required],
+      day: ['',Validators.required],
+      month: ['',Validators.required],
+      year:['',Validators.required],
+      gender:['',Validators.required]
+    })
+  }
+  submit() {
+    console.log(this.userFormGroup.value);
   }
 }
